477e27b71bf415af14a29ebd51f39830f50df698
Mason Garrison <garrissm@wfu.edu>
2025-05-05 23:07:13 -0400

    now?

--- R/calcCoordinates.R
+++ R/calcCoordinates.R
@@ -104,27 +104,27 @@ calculateConnections <- function(ped, type = c("siblings","parent-child", "spous
   # Separate mom and dad coordinates
   mom_connections <- connections %>%
     dplyr::filter(!is.na(momID)) %>%
-    left_join(ped, by = c("momID" = "personID"), suffix = c("", "_mom")) %>%
+    dplyr::left_join(ped, by = c("momID" = "personID"), suffix = c("", "_mom")) %>%
     rename(x_mom = x_pos_mom, y_mom = y_pos_mom) %>%
     select(personID, momID, x_mom, y_mom)
 
   dad_connections <- connections %>%
     dplyr::filter(!is.na(dadID)) %>%
-    left_join(ped, by = c("dadID" = "personID"), suffix = c("", "_dad")) %>%
+    dplyr::left_join(ped, by = c("dadID" = "personID"), suffix = c("", "_dad")) %>%
     rename(x_dad = x_pos_dad, y_dad = y_pos_dad) %>%
     select(personID, dadID, x_dad, y_dad)
 
   spouse_connections <- ped %>%
     select(personID, x_pos, y_pos, spouseID) %>%
-    left_join(ped, by = c("spouseID" = "personID"), suffix = c("", "_spouse")) %>%
+    dplyr::left_join(ped, by = c("spouseID" = "personID"), suffix = c("", "_spouse")) %>%
     rename(x_spouse = x_pos_spouse, y_spouse = y_pos_spouse) %>%
     select(personID, spouseID, x_spouse, y_spouse)
 
   # Combine mom and dad connections with the original dataset
   connections <- connections %>%
-    left_join(mom_connections, by = join_by(personID, momID)) %>%
-    left_join(dad_connections, by = join_by(personID, dadID)) %>%
-    left_join(spouse_connections, by = join_by(personID, spouseID))
+    dplyr::left_join(mom_connections, by = join_by(personID, momID)) %>%
+    dplyr::left_join(dad_connections, by = join_by(personID, dadID)) %>%
+    dplyr::left_join(spouse_connections, by = join_by(personID, spouseID))
 
   # Create midpoints for parents
   parent_midpoints <- connections %>%
@@ -147,7 +147,7 @@ calculateConnections <- function(ped, type = c("siblings","parent-child", "spous
 
   # Calculate midpoints for siblings
   sibling_midpoints <- connections %>%
-    filter(!is.na(dadID) & !is.na(momID)) %>%
+    dplyr::filter(!is.na(dadID) & !is.na(momID)) %>%
     group_by(dadID, momID) %>%
     summarize(
       x_mid_sib = mean(x_pos),
@@ -158,9 +158,9 @@ calculateConnections <- function(ped, type = c("siblings","parent-child", "spous
 
   # Merge midpoints back to connections
   connections <- connections %>%
-    left_join(parent_midpoints, by = c("dadID", "momID")) %>%
-  left_join(spouse_midpoints, by = join_by(spouseID))  %>%
-  left_join(sibling_midpoints, by = join_by(dadID, momID)) %>%
+    dplyr::left_join(parent_midpoints, by = c("dadID", "momID")) %>%
+    dplyr::left_join(spouse_midpoints, by = join_by(spouseID))  %>%
+    dplyr::left_join(sibling_midpoints, by = join_by(dadID, momID)) %>%
     mutate(
       x_mid_sib = case_when(is.na(x_mid_sib) & !is.na(dadID) & !is.na(momID) ~ x_pos,
                             !is.na(x_mid_sib) ~ x_mid_sib,
