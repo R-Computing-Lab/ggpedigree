8a96fb87e144daff2424c83b0f63959be4bcf7e2
Mason Garrison <garrissm@wfu.edu>
2025-05-06 20:22:12 -0400

    reknit

--- vignettes/plots.R
+++ vignettes/plots.R
@@ -5,10 +5,13 @@ knitr::opts_chunk$set(
   fig.width = 7,
   fig.height = 5
 )
+
+## ----libraries, message=FALSE, warning=FALSE----------------------------------
 library(ggBGmisc) # ggPedigree lives here
 library(BGmisc) # helper utilities & example data
 library(ggplot2) # ggplot2 for plotting
 library(viridis) # viridis for color palettes
+library(tidyverse) # for data wrangling
 
 ## -----------------------------------------------------------------------------
 data("potter")
@@ -28,15 +31,7 @@ ggPedigree(
     spouse_segment_color = "pink",
     sibling_segment_color = "blue",
     parent_segment_color = "green",
-    offspring_segment_color = "black",
-    text_size = 3,
-    point_size = 4,
-    line_width = 0.5,
-    generation_gap = 1,
-    sex_unknown_code = NA,
-    unknown_shape = 18,
-    female_shape = 16,
-    male_shape = 15
+    offspring_segment_color = "black"
   )
 )
 
@@ -47,4 +42,40 @@ ggPedigree(potter,
   code_male = 1
 ) +
   theme_bw(base_size = 12)
+
+## -----------------------------------------------------------------------------
+data("hazard")
+
+p <- ggPedigree(
+  hazard %>% mutate(affected = as.factor(ifelse(affected == TRUE, "affected", "unaffected"))),
+  famID_col = "famID",
+  personID_col = "ID",
+  code_male = 0,
+  status_col = "affected"
+)
+
+p
+
+## -----------------------------------------------------------------------------
+p +
+  facet_wrap(~famID, scales = "free_x")
+
+## -----------------------------------------------------------------------------
+p +
+  theme_bw(base_size = 12) +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    axis.line        = element_line(colour = "black"),
+    axis.text.x      = element_blank(),
+    axis.text.y      = element_blank(),
+    axis.ticks.x     = element_blank(),
+    axis.ticks.y     = element_blank(),
+    axis.title.x     = element_blank(),
+    axis.title.y     = element_blank()
+  ) + scale_color_viridis(
+    discrete = TRUE,
+    labels = c("Female", "Male", "Unknown")
+  )
 
