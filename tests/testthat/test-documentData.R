77a5a32c63421063e223a1dfa8a7afd7be2ac9c8
Mason Garrison <garrissm@wfu.edu>
2025-07-03 14:10:19 -0400

    Update test-documentData.R

--- tests/testthat/test-documentData.R
+++ tests/testthat/test-documentData.R
@@ -16,7 +16,7 @@ test_that("test that data loads", {
 test_that("ASOIAF data loads", {
 
   expect_silent(data(ASOIAF))
-  expect_true(nrow(ASOIAF) > 611)
+  expect_true(nrow(ASOIAF) > 600)
   expect_true(nrow(ASOIAF) == max(ASOIAF$id, na.rm = TRUE))
   checkis_acyclic <- BGmisc::checkPedigreeNetwork(ASOIAF,
                                           personID = "id",
