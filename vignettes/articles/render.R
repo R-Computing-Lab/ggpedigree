9915f8461f66a4ee42390cb2504c35596da726e7
Mason Garrison <garrissm@wfu.edu>
2025-07-06 14:56:14 -0400

    render doc

---
+++ vignettes/articles/render.R
@@ -0,0 +1,31 @@
+# Load required package
+library(rmarkdown)
+
+# Define file paths
+input_dir <- "vignettes/articles"
+input_file <- file.path(input_dir, "_paper.Rmd")  # Update filename if needed
+output_dir <- input_dir  # Same as input location
+
+# Render md_document
+#render(
+#  input = input_file,
+#  output_format = "md_document",
+#  output_file = "paper.md",
+#  output_dir = output_dir
+#)
+
+# Render rticles::joss_article
+render(
+  input = input_file,
+  output_format = "rticles::joss_article",
+  output_file = "paper.pdf",
+  output_dir = output_dir
+)
+
+# Render rmarkdown::html_vignette
+#render(
+#  input = input_file,
+#  output_format = "rmarkdown::html_vignette",
+#  output_file = "ggpedigree_vignette.html",
+#  output_dir = output_dir
+#)
